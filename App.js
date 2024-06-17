f511f8dd5236d4d51c66e64538f0c9e7612dd07a
Banisco8 <banibagodsway81@gmail.com>
2024-06-17 11:35:48 +0100

    Added the README file and also added screenshots to the README file

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ export default function App() {
       <Stack.Screen name="Home" component={HomeScreen}
       options={{headerShown: false}} />
     </Stack.Navigator>
+    
   </NavigationContainer>
   );
 }
